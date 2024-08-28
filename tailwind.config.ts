7f28dcb0f96af5a57f9aa856ae7e2f74c9bb0414
Hugo Reis <hbssreis@gmail.com>
2024-08-28 16:13:07 +0100

    fix: removed extended 'xs screen'

--- tailwind.config.ts
+++ tailwind.config.ts
@@ -23,9 +23,6 @@ const config: Config = {
         "gradient-conic":
           "conic-gradient(from 180deg at 50% 50%, var(--tw-gradient-stops))",
       },
-      screens: {
-        "xs": "375px",
-      },
     },
   },
   plugins: [
